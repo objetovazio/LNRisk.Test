09468099cd8a65c141833f32fe1a75fd6e295ddf
André Martins <objetovazio@gmail.com>
2019-02-16 10:24:50 -0200

    Item.cs with string properties MyClass.cs with control methods Program.cs to testing those methods

---
+++ LNRisk.Test.Business/Myclass.cs
@@ -0,0 +1,140 @@
+﻿using LNRisk.Test.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LNRisk.Test.Business
+{
+    public class Myclass
+    {
+        private List<Item> myData;
+
+        public Myclass()
+        {
+            myData = new List<Item>();
+        }
+
+        public void StoreData(string id, string payload)
+        {
+            var newItem = new Item
+            {
+                Id = id,
+                Payload = payload
+            };
+
+            this.myData.Add(newItem);
+        }
+
+        public bool RemoveData(string id)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+
+            if (item != null)
+            {
+                myData.Remove(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetPayload(string id)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+
+            if (item != null) return item.Payload;
+
+            return string.Empty;
+        }
+
+        public bool EditData(string id, string newPayload)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+
+            if (item != null)
+            {
+                item.Payload = newPayload;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CountDates(string id)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+
+            Regex dateRegex = new Regex("\\d{4}/\\d{2}/\\d{2}");
+
+            return dateRegex.Matches(item.Payload).Count;
+        }
+
+        public Dictionary<char, int> CountLetters(string id)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+            var dictCountedLetters = new Dictionary<char, int>();
+
+            foreach (char charc in item.Payload)
+            {
+                if (dictCountedLetters.ContainsKey(charc))
+                {
+                    dictCountedLetters[charc]++;
+                }
+                else if (char.IsLetter(charc))
+                {
+                    dictCountedLetters.Add(charc, 1);
+                }
+            }
+
+            return dictCountedLetters;
+        }
+
+        public List<string> SearchPayload(string payload)
+        {
+            return this.myData
+                .Where(x => x.Payload.Equals(payload)) // Filter all items that has the same payload
+                .Select(x => x.Id).ToList(); // Put all id's filteres into a list
+        }
+
+        public string SearchBiggestPalindrome(string id)
+        {
+            var item = this.myData.FirstOrDefault(x => x.Id.Equals(id));
+
+            string[] allWords = item.Payload.Split(' ');
+            string biggestPalindrome = string.Empty;
+
+
+            foreach (string word in allWords)
+            {
+                if (IsPalindrome(word))
+                {
+                    if (biggestPalindrome.Length < word.Length)
+                        biggestPalindrome = word;
+                }
+            }
+
+            return biggestPalindrome;
+        }
+
+        private bool IsPalindrome(string word)
+        {
+            int wordLenght = word.Length-1;
+            var palindrome = true;
+            word = word.ToLower();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!word[i].Equals(word[wordLenght - i]))
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+
+            return palindrome;
+        }
+    }
+}
---
+++ LNRisk.Test.ConsoleTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using LNRisk.Test.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNRisk.Test.ConsoleTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Myclass myClass = new Myclass();
+
+            // Creating First Data
+            // Testing the StoreData()
+            myClass.StoreData("1", "arara");
+            myClass.StoreData("2", "Anna");
+            myClass.StoreData("3", "Civic");
+            myClass.StoreData("5", "Kayak");
+            myClass.StoreData("6", "Level");
+            myClass.StoreData("7", "Madam");
+            myClass.StoreData("8", "On 2014/03/22 I was testing the hello word. And now on 2019/02/15, I'm testing this.");
+            myClass.StoreData("9", "Level");
+            myClass.StoreData("10", "Civic Noon Madam Kayak tattarrattat");
+
+            // Testing RemoveData()
+            if (myClass.RemoveData("1"))
+            {
+                Console.WriteLine("Data removed.");
+            }
+
+            // Testing GetPayload()
+            Console.WriteLine("Payload 2: " + myClass.GetPayload("2"));
+
+            // Testing EditPaylod()
+            if (myClass.EditData("2", "NewPayload is Loaded"))
+            {
+                Console.WriteLine("Edited Payload 2: " + myClass.GetPayload("2"));
+            }
+
+            // Testing CountDates()
+            Console.WriteLine("Count Dates from Id 8: " + myClass.CountDates("8")); //TODO: Check this again
+
+            // Testing CountLetters()
+            Console.WriteLine("Count Letters from Id 8:"); //TODO: Check this again
+            var dict = myClass.CountLetters("8");
+
+            foreach (var dictKey in dict.Keys)
+            {
+                var value = dict[dictKey];
+                Console.Write(string.Format("|{0} = {1}| ", dictKey.ToString(), value));    
+            }
+            Console.WriteLine("");
+
+
+            //Testing SearchPayload()
+            Console.Write("Search Payload 'Level': ");
+            List<string> ids = myClass.SearchPayload("Level");
+            foreach (var id in ids)
+            {
+                Console.Write("| " + id + " | ");
+            }
+            Console.WriteLine("");
+
+            // Testing SearchBiggestPalindrome()
+            Console.WriteLine("Biggest Palindrome found 10: " + myClass.SearchBiggestPalindrome("10"));
+
+            Console.ReadKey();
+        }
+    }
+}
